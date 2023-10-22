4a62705351eee6886a9956212e70b28c0a6ac75e
therealmal <44868259+TheRealMal@users.noreply.github.com>
2023-10-22 17:13:33 +0300

    feat: added StartServer func

--- pkg/vulners/router.go
+++ pkg/vulners/router.go
@@ -3,6 +3,10 @@ package vulners
 import (
 	"context"
 	"grpc-nmap-vulners/pkg/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 // Server for functions implementation
@@ -13,4 +17,20 @@ type Server struct {
 // CheckVuln call implementation
 func (s *Server) CheckVuln(_ context.Context, req *proto.CheckVulnRequest) (*proto.CheckVulnResponse, error) {
 	return requestScanWithNmap(req)
+}
+
+// StartServer runs gRPC call listener
+func StartServer(network string, address string) {
+	l, err := net.Listen(network, address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	srv := &Server{}
+	proto.RegisterNetVulnServiceServer(grpcServer, srv)
+
+	if err := grpcServer.Serve(l); err != nil {
+		log.Fatal(err)
+	}
 }
